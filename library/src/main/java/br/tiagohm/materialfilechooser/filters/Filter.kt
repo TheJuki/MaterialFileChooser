7ed368dd7ecfd417d4f7e018cb2e4c9b43b4d1e2
tiagohm <tiago.henrique.cco@gmail.com>
2018-05-12 18:17:29 -0300

    Fix #22: Mover para a linguagem Kotlin

---
+++ library/src/main/java/br/tiagohm/materialfilechooser/filters/Filter.kt
@@ -0,0 +1,15 @@
+package br.tiagohm.materialfilechooser.filters
+
+import java.io.File
+import java.io.FileFilter
+
+abstract class Filter(private val filter: Filter? = null) : FileFilter {
+    
+    open fun filter(file: File): Boolean {
+        return true
+    }
+    
+    final override fun accept(file: File): Boolean {
+        return filter(file) && (filter?.accept(file) ?: true)
+    }
+}
